9b5b121d5d84a603bc076d6517598cf7f7eeae7b
Bear <bear@Bears-MacBook-Pro.local>
2022-04-19 19:56:47 -0400

    beginnings of JFrame structure, including logo for frame

--- code/Bank.java
+++ code/Bank.java
@@ -1,8 +1,28 @@
 import java.io.BufferedReader;
 import java.io.FileWriter;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 
 /** TODO javadoc
  *
  *
  *
  * */
+
+public class Bank {
+	
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame(); // create frame
+		frame.setTitle("Bank of UMW"); // title
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit on x
+		frame.setResizable(false); // can't resize
+		frame.setSize(500,500); // size
+		frame.setVisible(true); // display it
+
+		ImageIcon image = new ImageIcon("logoBankOfUMW.png"); // create image icon
+		frame.setIconImage(image.getImage()); // assign image icon
+	} // close main method
+
+
+} // close Bank class
