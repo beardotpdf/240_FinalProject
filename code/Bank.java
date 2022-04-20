de7a319508d6d59ebf906e2970c4e691871de6fd
Bear <bear@Bears-MacBook-Pro.local>
2022-04-19 20:40:54 -0400

    changed mind about split, continue work, remove BankFrame

--- code/Bank.java
+++ code/Bank.java
@@ -1,5 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileWriter;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.Color;
 /**
 * 
 * @param args
@@ -8,8 +13,20 @@ import java.io.FileWriter;
 public class Bank {
 	
 	public static void main(String[] args) {
+
+		ImageIcon logo = new ImageIcon("logoBankOfUMW.png");
+
+		JLabel welcome = new JLabel("Welcome to Bank of UMW");
+		JLabel instructions = new JLabel("Please Select an Option Below");
 		
-		BankFrame frame = new BankFrame();
+		JFrame frame = new JFrame();
+		frame.setIconImage(logo.getImage());
+		frame.add(welcome);
+		frame.add(instructions);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(650,650);
+		frame.setResizable(false);
+		frame.setVisible(true);
 
 	} // close main method
 
